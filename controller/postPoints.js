99c43041d630431f1c7668d60fc47748dd2d3d12
shanawaz-Git <flamefayaz03@gmail.com>
2023-05-13 21:28:43 +0530

    moved instaclient script before res send

--- controller/postPoints.js
+++ controller/postPoints.js
@@ -6,8 +6,8 @@ const { igImageUpload, instaClient } = require("../helper/instaconnect");
 exports.IGpost = async (req, res, next) => {
   try {
     var resObj = await igImageUpload();
-    res.status(resObj.code).json(resObj.message);
     await instaClient(resObj.imageBuffer, resObj.captionWithHashTag);
+    res.status(resObj.code).json(resObj.message);
   } catch (error) {
     res.status(400).json({
       code: 400,
