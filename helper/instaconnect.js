2ecd4b87b3bf2a29fdf33c0c175449b0d5417a18
shanawaz-Git <flamefayaz03@gmail.com>
2023-05-13 18:29:53 +0530

    initialize and end post to dec.time

--- helper/instaconnect.js
+++ helper/instaconnect.js
@@ -4,15 +4,11 @@ require("dotenv").config();
 const { IgApiClient } = require("instagram-private-api");
 const axios = require("axios");
 const { quotes } = require("./quotesAPI");
-const fs = require("fs");
 const { randomHashtag } = require("./hashtags");
 
 exports.igImageUpload = async () => {
   var res = {};
   try {
-    const ig = new IgApiClient();
-    ig.state.generateDevice(process.env.UNAME);
-    await ig.account.login(process.env.UNAME, process.env.PASSWORD);
     var quote;
     var quoteRes = quotes();
     await quoteRes.then(function (result) {
@@ -28,20 +24,12 @@ exports.igImageUpload = async () => {
       `https://web-series-quotes-api.deta.dev/pic/custom?text=${text}&background_color="white"&text_color="black"&text_size=50&x=1080&y=1080`,
       { responseType: "arraybuffer" }
     );
-    //----------------------------------------------------------
-    await ig.publish
-      .photo({
-        file: imageBuffer.data,
-        caption: captionWithHashTag,
-      })
-      .then((ans) => {
-        //----------------------------------------------------------
-        res = {
-          code: 200,
-          status: "Success",
-          message: ans.status,
-        };
-      });
+    res = instaClient(imageBuffer, captionWithHashTag);
+    res = {
+      code: 200,
+      status: "Success",
+      message: "Publishing initialized",
+    };
   } catch (error) {
     res = {
       code: 403,
@@ -52,29 +40,41 @@ exports.igImageUpload = async () => {
   return res;
 };
 
-function getTextHeight(text, context, maxWidth) {
-  const words = text.split(" ");
-  let line = "";
-  let lines = 1;
-  const lineHeight = context.measureText("M").width * 1.2;
-  for (let n = 0; n < words.length; n++) {
-    const testLine = line + words[n] + " ";
-    const metrics = context.measureText(testLine);
-    const testWidth = metrics.width;
-    if (testWidth > maxWidth && n > 0) {
-      line = words[n] + " ";
-      lines++;
-    } else {
-      line = testLine;
-    }
-  }
-  return lines * lineHeight;
-}
+// function getTextHeight(text, context, maxWidth) {
+//   const words = text.split(" ");
+//   let line = "";
+//   let lines = 1;
+//   const lineHeight = context.measureText("M").width * 1.2;
+//   for (let n = 0; n < words.length; n++) {
+//     const testLine = line + words[n] + " ";
+//     const metrics = context.measureText(testLine);
+//     const testWidth = metrics.width;
+//     if (testWidth > maxWidth && n > 0) {
+//       line = words[n] + " ";
+//       lines++;
+//     } else {
+//       line = testLine;
+//     }
+//   }
+//   return lines * lineHeight;
+// }
 
-const getRandomImage = async () => {
-  const accessKey = "M0Wo-e_TdXprLnmMFL78baXWARNwe49Wr3qQMf2lZoY"; // replace with your own Unsplash access key
-  const randomUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}`;
-  const response = await axios.get(randomUrl);
-  // console.log(response.data.urls);
-  return response.data.urls.regular;
+// const getRandomImage = async () => {
+//   const accessKey = "M0Wo-e_TdXprLnmMFL78baXWARNwe49Wr3qQMf2lZoY"; // replace with your own Unsplash access key
+//   const randomUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}`;
+//   const response = await axios.get(randomUrl);
+//   // console.log(response.data.urls);
+//   return response.data.urls.regular;
+// };
+
+const instaClient = async (imageBuffer, captionWithHashTag) => {
+  res = {};
+  const ig = new IgApiClient();
+  ig.state.generateDevice(process.env.UNAME);
+  ig.account.login(process.env.UNAME, process.env.PASSWORD).then(() => {
+    ig.publish.photo({
+      file: imageBuffer.data,
+      caption: captionWithHashTag,
+    });
+  });
 };
