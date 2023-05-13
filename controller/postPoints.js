3b932e21b5571047ef19368e2eb2e362d2f12d6c
shanawaz-Git <flamefayaz03@gmail.com>
2023-05-13 21:42:29 +0530

    remove cmnt & remove await for ig

--- controller/postPoints.js
+++ controller/postPoints.js
@@ -6,7 +6,7 @@ const { igImageUpload, instaClient } = require("../helper/instaconnect");
 exports.IGpost = async (req, res, next) => {
   try {
     var resObj = await igImageUpload();
-    await instaClient(resObj.imageBuffer, resObj.captionWithHashTag);
+    instaClient(resObj.imageBuffer, resObj.captionWithHashTag);
     res.status(resObj.code).json(resObj.message);
   } catch (error) {
     res.status(400).json({
--- helper/instaconnect.js
+++ helper/instaconnect.js
@@ -24,8 +24,6 @@ exports.igImageUpload = async () => {
       `https://web-series-quotes-api.deta.dev/pic/custom?text=${text}&background_color="white"&text_color="black"&text_size=50&x=1080&y=1080`,
       { responseType: "arraybuffer" }
     );
-    // res = instaClient(imageBuffer, captionWithHashTag);
-    // console.log("invoking instaclient");
     res = {
       code: 200,
       status: "Success",
@@ -42,33 +40,6 @@ exports.igImageUpload = async () => {
   }
   return res;
 };
-
-// function getTextHeight(text, context, maxWidth) {
-//   const words = text.split(" ");
-//   let line = "";
-//   let lines = 1;
-//   const lineHeight = context.measureText("M").width * 1.2;
-//   for (let n = 0; n < words.length; n++) {
-//     const testLine = line + words[n] + " ";
-//     const metrics = context.measureText(testLine);
-//     const testWidth = metrics.width;
-//     if (testWidth > maxWidth && n > 0) {
-//       line = words[n] + " ";
-//       lines++;
-//     } else {
-//       line = testLine;
-//     }
-//   }
-//   return lines * lineHeight;
-// }
-
-// const getRandomImage = async () => {
-//   const accessKey = "M0Wo-e_TdXprLnmMFL78baXWARNwe49Wr3qQMf2lZoY"; // replace with your own Unsplash access key
-//   const randomUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}`;
-//   const response = await axios.get(randomUrl);
-//   // console.log(response.data.urls);
-//   return response.data.urls.regular;
-// };
 
 exports.instaClient = async (imageBuffer, captionWithHashTag) => {
   console.log("invoked instaclient");
