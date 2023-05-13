d181e3c3790ddbc59093f81775f2e91d252a6d32
shanawaz-Git <flamefayaz03@gmail.com>
2023-05-13 16:41:51 +0530

    default port 5009

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const getRoute = require("./routes/getRoute");
 //----
 //----
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5009;
 //----
 app.use(cors());
 app.use(bodyParser.json());
